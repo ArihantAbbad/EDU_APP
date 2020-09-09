285b450889ce54e4478139564be871af22cd3470
Onkar_hanchate <onkarhanchate14@gmail.com>
2020-09-09 22:30:42 +0530

    portrait mode
    added portraite mode

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // device rotation off
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EDU APP',
